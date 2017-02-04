1f30674858ac7734cdef17ce7fee7787ca13675b
jess-bart <jessy-barthelemy@hotmail.fr>
2017-02-04 14:14:53 +0100

    style : login page

--- app/src/main/java/com/jessy_barthelemy/pictothemo/LoginActivity.java
+++ app/src/main/java/com/jessy_barthelemy/pictothemo/LoginActivity.java
@@ -1,13 +1,65 @@
 package com.jessy_barthelemy.pictothemo;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout email;
+    private TextInputLayout password;
+    private Button loginAction;
+    private Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        resources = getResources();
+        email = (TextInputLayout)findViewById(R.id.login_email);
+        password = (TextInputLayout)findViewById(R.id.login_password);
+        loginAction = (Button) findViewById(R.id.login_action);
+
+        password.getEditText().setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if (actionId == EditorInfo.IME_ACTION_GO
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+
+                    //Hide keyboard
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(LoginActivity.this.getCurrentFocus().getWindowToken(), 0);
+
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        loginAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptLogin();
+            }
+        });
+    }
+
+    private void attemptLogin(){
+        Toast.makeText(LoginActivity.this, "Attempting login", Toast.LENGTH_SHORT).show();
     }
 }
