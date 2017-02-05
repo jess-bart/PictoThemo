9ac437f57c6263888af230de095b5894650d1900
jess-bart <jessy-barthelemy@hotmail.fr>
2017-02-05 14:41:25 +0100

    adding registration form verification

---
+++ app/src/main/java/com/jessy_barthelemy/pictothemo/DatabaseInteractions/RegistrationTask.java
@@ -0,0 +1,57 @@
+package com.jessy_barthelemy.pictothemo.DatabaseInteractions;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.AsyncTask;
+import android.os.SystemClock;
+import android.support.design.widget.TextInputLayout;
+
+import com.jessy_barthelemy.pictothemo.R;
+
+public class RegistrationTask extends AsyncTask<String, Void, String> {
+
+    private TextInputLayout email;
+    private Resources resources;
+    private ProgressDialog waitDialog;
+
+    public RegistrationTask(Context ctx, TextInputLayout email){
+        this.email = email;
+        this.resources = ctx.getResources();
+
+        waitDialog = new ProgressDialog(ctx);
+        waitDialog.setMessage(resources.getString(R.string.login_verification));
+        waitDialog.setIndeterminate(false);
+        waitDialog.setCancelable(false);
+    }
+
+    /*
+    * Param 1 : Email(String)
+    * Param 2 : Password(String)
+    * Return the error message or null
+    * */
+    @Override
+    protected String doInBackground(String... params) {
+        SystemClock.sleep(7000);
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        waitDialog.show();
+    }
+
+    protected void onPostExecute(String result) {
+        waitDialog.dismiss();
+
+        if(result != null && !result.isEmpty()){
+            email.setErrorEnabled(true);
+            email.setError(result);
+            return;
+        }
+
+        email.setErrorEnabled(false);
+        email.setError(null);
+    }
+}
--- app/src/main/java/com/jessy_barthelemy/pictothemo/LoginActivity.java
+++ app/src/main/java/com/jessy_barthelemy/pictothemo/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.jessy_barthelemy.pictothemo;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.res.Resources;
+import android.net.ConnectivityManager;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -14,22 +16,35 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.jessy_barthelemy.pictothemo.DatabaseInteractions.RegistrationTask;
+
+import java.util.Hashtable;
+import java.util.regex.Pattern;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputLayout email;
     private TextInputLayout password;
     private Button loginAction;
+    private Button registerAction;
     private Resources resources;
+    /*Keep the last action to execute on password IME action (login or register)*/
+    private boolean attemptRegitration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        attemptRegitration = true;
         resources = getResources();
         email = (TextInputLayout)findViewById(R.id.login_email);
         password = (TextInputLayout)findViewById(R.id.login_password);
         loginAction = (Button) findViewById(R.id.login_action);
+        registerAction = (Button) findViewById(R.id.login_register_action);
 
         password.getEditText().setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -44,13 +59,22 @@ public class LoginActivity extends AppCompatActivity {
                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                     imm.hideSoftInputFromWindow(LoginActivity.this.getCurrentFocus().getWindowToken(), 0);
 
-                    attemptLogin();
+                    if(attemptRegitration)
+                        attemptRegistration();
+                    else
+                        attemptLogin();
                     return true;
                 }
                 return false;
             }
         });
 
+        registerAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptRegistration();
+            }
+        });
         loginAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,6 +84,55 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void attemptLogin(){
-        Toast.makeText(LoginActivity.this, "Attempting login", Toast.LENGTH_SHORT).show();
+        attemptRegitration = false;
+        //Email verification
+        if(!validateEmail(email.getEditText().getText().toString())){
+            email.setErrorEnabled(true);
+            email.setError(resources.getString(R.string.login_email_verification));
+            return;
+        }
+
+        email.setError(null);
+        email.setErrorEnabled(false);
+
+        //Password verification
+        if(!validatePassword(password.getEditText().getText().toString())){
+            password.setErrorEnabled(true);
+            password.setError(resources.getString(R.string.login_password_verification));
+            return;
+        }
+
+        password.setError(null);
+        password.setErrorEnabled(false);
+
+        /*Database verification*/
+        RegistrationTask registration = new RegistrationTask(this, email);
+        registration.execute(email.getEditText().getText().toString(), password.getEditText().getText().toString());
     }
+
+    public boolean validateEmail(String email){
+        try{
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+            return true;
+        }catch(AddressException e){
+            return false;
+        }
+    }
+
+    public boolean validatePassword(String password){
+        return password.length() >= 8;
+    }
+
+    private void attemptRegistration(){
+        Toast.makeText(LoginActivity.this, "Attempting registration", Toast.LENGTH_SHORT).show();
+        attemptRegitration = true;
+    }
+
+    /*
+    *     public boolean isNetworkAvailable() {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+    * */
 }
